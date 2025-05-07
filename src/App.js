71f429baae694ad4978a1feaca3265aa1ecc41c8
Yusri Hilmi <yusrihilmi4@gmail.com>
2025-05-07 12:57:35 +0700

    update latest

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Question from "./pages/QuestionLadder/question";
+import Leadership from "./pages/GameDLS/leadership";
+import ServiceExcellence from "./pages/GameDLS/service-excellence";
 import Login from "./pages/Login";
 import RequireAuth from "./components/RequireAuth";
 import Layout from "./components/Layout";
@@ -11,6 +14,9 @@ import NHIE from "./pages/Games/nhie";
 const App = () => (
   <BrowserRouter>
     <Routes>
+    <Route path="/question" element={<Question />} />
+    <Route path="/leadership" element={<Leadership />} />
+    <Route path="/service-excellence" element={<ServiceExcellence />} />
       <Route path="/login" element={<Login />} />
       <Route
         element={
